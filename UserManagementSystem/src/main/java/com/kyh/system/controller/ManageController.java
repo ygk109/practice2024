a40a9d776d2494f4585fe0483c562ac275fe7715
ygk109 <ygk5192@gmail.com>
2024-09-28 18:15:47 +0900

    all page(3page) 完成

--- UserManagementSystem/src/main/java/com/kyh/system/controller/ManageController.java
+++ UserManagementSystem/src/main/java/com/kyh/system/controller/ManageController.java
@@ -1,18 +1,12 @@
 package com.kyh.system.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.kyh.system.service.UserService;
-
 @Controller
 @RequestMapping(value = "manage")
 public class ManageController {
-
-    @Autowired
-    private UserService userService;
 
     @RequestMapping(value = "/", method = {RequestMethod.POST, RequestMethod.GET})
     public String manage() {
